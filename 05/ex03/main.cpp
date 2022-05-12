d1227764b0cfacf42d458ee4bf8568cc072400b1
gyujun9403 <gujun112@naver.com>
2022-05-12 10:26:22 +0900

    c05 ex03 main

--- 05/ex03/main.cpp
+++ 05/ex03/main.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+	Bureaucrat gyeon("gyeon", 35);
 	Intern someRandomIntern;
 	Form* rrf;
 	Form* scf;
@@ -11,10 +12,26 @@ int main()
 
 	rrf = someRandomIntern.makeForm("robotomy request", "Marvin");
 	std::cout << *rrf << std::endl;
+	std::cout << *dynamic_cast<RobotomyRequestForm*>(rrf) << std::endl;
 	scf = someRandomIntern.makeForm("shrubbery creation", "Jack");
 	std::cout << *scf << std::endl;
+	std::cout << *static_cast<ShrubberyCreationForm*>(scf) << std::endl;
 	ppf = someRandomIntern.makeForm("presidential pardon", "Leon");
 	std::cout << *ppf << std::endl;
+	std::cout << *static_cast<PresidentialPardonForm*>(ppf) << std::endl;
+	try
+	{
+		gyeon.signForm(*dynamic_cast<RobotomyRequestForm*>(rrf));
+		gyeon.executeForm(*dynamic_cast<RobotomyRequestForm*>(rrf));
+		(*dynamic_cast<RobotomyRequestForm*>(rrf)).beSigned(gyeon);
+		(*dynamic_cast<RobotomyRequestForm*>(rrf)).execute(gyeon);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
 	free(rrf);
 	free(scf);
 	free(ppf);
